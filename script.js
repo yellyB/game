b35c512ae8babe9391a3efc30bcede336da46b8a
김보미 <kk3p1q1@gmail.com>
2023-02-25 16:26:08 +0900

    canvas 세팅

---
+++ script.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+console.log(ctx);
+
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
+
+const playerImage = new Image();
+playerImage.src = "shadow_dog.png";
+let x = 0;
+
+function anumate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillRect(x, 50, 100, 100);
+  x++; // 우측으로 계속 이동
+  requestAnimationFrame(animate); // loop
+}
+animate();
