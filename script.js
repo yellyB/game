6221bca31463d9a3f1feee03f3ded7356adcc9dd
김보미 <kk3p1q1@gmail.com>
2023-02-26 22:36:02 +0900

    render dog animation

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ playerImage.src = "shadow_dog.png";
 let x = 0;
 const spriteWidth = 575; // image width=6876px 이기때문에, image 칸 수인 12로 나눈 값이 이미지 하나의 width가 됨
 const spriteHeight = 523; // 5230px / 10
+let frameX = 0;
+let frameY = 0;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -19,8 +21,8 @@ function animate() {
   // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
   ctx.drawImage(
     playerImage,
-    0,
-    0,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
     0,
@@ -28,6 +30,13 @@ function animate() {
     CANVAS_WIDTH,
     CANVAS_HEIGHT
   );
+
+  // idx 0 의 row 가 7개 이미지이기 때문에 7이하일때만 증가, 넘어가면 초기화
+  if (frameX < 6) {
+    frameX++;
+  } else {
+    frameX = 0;
+  }
   requestAnimationFrame(animate); // loop
 }
 animate();
