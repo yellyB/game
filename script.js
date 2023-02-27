f94390f4e9b87d0ea9d018e3f77c21500b97b801
김보미 <kk3p1q1@gmail.com>
2023-02-27 23:55:43 +0900

    animation 속도 조절

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ playerImage.src = "shadow_dog.png";
 let x = 0;
 const spriteWidth = 575; // image width=6876px 이기때문에, image 칸 수인 12로 나눈 값이 이미지 하나의 width가 됨
 const spriteHeight = 523; // 5230px / 10
-let frameX = 0;
-let frameY = 0;
+let frameX = 0; // 이미지의 col
+let frameY = 0; // 이미지의 row
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -31,12 +33,16 @@ function animate() {
     CANVAS_HEIGHT
   );
 
-  // idx 0 의 row 가 7개 이미지이기 때문에 7이하일때만 증가, 넘어가면 초기화
-  if (frameX < 6) {
-    frameX++;
-  } else {
-    frameX = 0;
+  // 애니메이션 속도 조절. staggerFrames 이 증가할 수록 나머지가 0이 되는 타이밍이 늦어짐. = 속도 느려짐
+  if (gameFrame % staggerFrames == 0) {
+    // idx 0 의 row 가 7개 이미지이기 때문에 7이하일때만 증가, 넘어가면 초기화
+    if (frameX < 6) {
+      frameX++;
+    } else {
+      frameX = 0;
+    }
   }
+  gameFrame++;
   requestAnimationFrame(animate); // loop
 }
 animate();
