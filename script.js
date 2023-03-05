00d6b75cdf70394fcaa4d97ac90384d95bba4c36
김보미 <kk3p1q1@gmail.com>
2023-03-05 17:00:12 +0900

    enemy movement 구현 준비

---
+++ script.js
@@ -0,0 +1,17 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+
+enemy1 = { x: 10, y: 50, width: 200, height: 200 };
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemy1.x++;
+  enemy1.y++;
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+  requestAnimationFrame(animate);
+}
+animate();
