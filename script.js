b18875827acf8d0499d166009f4b775e4f19601e
김보미 <kk3p1q1@gmail.com>
2023-03-06 20:26:47 +0900

    enemy random으로 생성

--- script.js
+++ script.js
@@ -5,13 +5,59 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
 
-enemy1 = { x: 10, y: 50, width: 200, height: 200 };
+const numverOfEnemies = 100;
+const enemiesArray = [];
+
+const enemyImage = new Image();
+enemyImage.src = "enemies/enemy1.png";
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.speed = Math.random() * 4 - 2; // -2 ~ 2까지의 숫자 얻게 됨
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0;
+  }
+  update() {
+    this.x += this.speed;
+    this.y += this.speed;
+    this.frame > 4 ? (this.frame = 0) : this.frame++; // 5개의 이미지를 루프
+  }
+  draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      enemyImage,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+
+for (let i = 0; i < numverOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.x++;
-  enemy1.y++;
-  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+
   requestAnimationFrame(animate);
 }
 animate();
