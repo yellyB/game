ab71921ad39370ac2bc92fb5958c8cd02b4814a6
김보미 <kk3p1q1@gmail.com>
2023-03-09 19:27:12 +0900

    movement pattern 3: (WIP)

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = "enemies/enemy2.png";
+    this.image.src = "enemies/enemy3.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.width = this.spriteWidth / 2.5; // 나눠주어서 enemy 크기 조절
     this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (canvas.width - this.width); // enemy가 내부에만 들어오도록(경계에 걸치지 않게) this.width빼줌
@@ -27,12 +27,18 @@ class Enemy {
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     this.angle = Math.random() * 2;
-    this.angleSpeed = Math.random() * 0.2; // 0~1사이. 클수록 웨이브 순환(?)간격 짧아짐
-    this.curve = Math.random() * 3; // -3 ~ +3, 높을수록 웨이브 깊어짐. 게임 난이도 올라감
+    this.angleSpeed = Math.random() * 2 + 0.5; // 멀면 더 빨리 돌도록 특정 값을 더해줌(가속)
+    this.curve = Math.random() * 200 + 50; //  높을수록 이동 간격 넓어짐. 게임 난이도 올라감
   }
   update() {
-    this.x -= this.speed;
-    this.y += Math.sin(this.angle) * this.curve;
+    // x y 둘 다 cos 을 사용한다면 왔다갔다하는 움직임을 보일것임
+    // x y 를 sin|cos 함수 내에서 같은 앵글 값으로 나눠주면 원으로 움직임
+    this.x =
+      this.curve * Math.sin((this.angle * Math.PI) / 90) +
+      (canvas.width / 2 - this.width / 2); // 화면 내에서 좌우로 움직이도록
+    this.y =
+      this.curve * Math.cos((this.angle * Math.PI) / 270) +
+      (canvas.height / 2 - this.height / 2); // 화면 내에서 상하로 움직이도록
     this.angle += this.angleSpeed;
 
     // 왼쪽으로 완전 없어지면 오른쪽에 다시 나타남
