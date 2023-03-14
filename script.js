99a00958447ed65f3630c65f0fa55c3d2efd5356
김보미 <kk3p1q1@gmail.com>
2023-03-14 23:55:25 +0900

    까마귀 날개짓+랜덤

--- script.js
+++ script.js
@@ -11,18 +11,46 @@ let ravens = [];
 
 class Raven {
   constructor() {
-    this.width = 100;
-    this.height = 50;
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
+    this.sizeModifier = Math.random() * 0.6 + 0.4;
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
     this.x = canvas.width;
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
     this.directionX = Math.random() * 5 - 2.5;
+    this.markedForDeletion = false;
+    this.image = new Image();
+    this.image.src = "images/raven.png";
+    this.frame = 0;
+    this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = 100;
   }
-  update() {
+  update(deltaTime) {
     this.x -= this.directionX;
+    if (this.x < 0 - this.width) this.markedForDeletion = true;
+
+    this.timeSinceFlap += deltaTime;
+    if (this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
+    }
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
@@ -33,7 +61,13 @@ function animate(timestamp) {
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
   timeToNextRaven += deltaTime;
-
+  if (timeToNextRaven > ravenInterval) {
+    ravens.push(new Raven());
+    timeToNextRaven = 0;
+  }
+  [...ravens].forEach((object) => object.update(deltaTime));
+  [...ravens].forEach((object) => object.draw());
+  ravens = ravens.filter((object) => !object.markedForDeletion);
   requestAnimationFrame(animate);
 }
-animate();
+animate(0);
