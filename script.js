3c3b00bd02c64608bdcf9cad9a9cdc02867c568d
김보미 <kk3p1q1@gmail.com>
2023-03-21 11:25:23 +0900

    left, right keyboard event

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   canvas.width = 800;
   canvas.height = 720;
 
-  console.log("fewejk");
   class InputHandler {
     constructor() {
       this.keys = [];
       window.addEventListener("keydown", (e) => {
-        console.log(e);
         if (
           (e.key === "ArrowDown" ||
             e.key === "ArrowUp" ||
@@ -21,7 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
       window.addEventListener("keyup", (e) => {
-        console.log(e);
         if (
           e.key === "ArrowDown" ||
           e.key === "ArrowUp" ||
@@ -34,7 +31,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  class Player {}
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 10;
+      this.y = this.gameHeight - this.height;
+      this.image = document.getElementById("playerImage");
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
+    }
+    draw(context) {
+      context.fillStyle = "white";
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+    update(input) {
+      // horizontal movement
+      this.x += this.speed;
+      if (input.keys.indexOf("ArrowRight") > -1) {
+        this.speed = 5;
+      } else if (input.keys.indexOf("ArrowLeft") > -1) {
+        this.speed = -5;
+      } else {
+        this.speed = 0;
+      }
+    }
+  }
 
   class Background {}
 
@@ -45,15 +81,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const displayStatusText = () => {};
 
   const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
 
   const animate = () => {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // const deltaTime = timeStamp - lastTime;
-    // lastTime = timeStamp;
-    // game.update(deltaTime);
-    // game.draw();
-    // requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update(input);
+    requestAnimationFrame(animate);
   };
-
-  // animate(0);
+  animate();
 });
