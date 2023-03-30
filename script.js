a78385fec268d2e41cdef96ec77ae44f3883aa73
김보미 <kk3p1q1@gmail.com>
2023-03-30 12:13:43 +0900

    left, right 키 핸들링

---
+++ script.js
@@ -0,0 +1,25 @@
+import Player from "./player.js";
+import InputHandler from "./input.js";
+import { drawStatusText } from "./utils.js";
+
+window.addEventListener("load", () => {
+  const loading = document.getElementById("loading");
+  loading.style.display = "none";
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  const player = new Player(canvas.width, canvas.height);
+
+  const input = new InputHandler();
+
+  const animate = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update(input.lastKey);
+    player.draw(ctx);
+    drawStatusText(ctx, input, player);
+    requestAnimationFrame(animate);
+  };
+  animate();
+});
