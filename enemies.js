dd74ab4f734ce5632ec9a9f9a973cd7e72e01150
김보미 <kk3p1q1@gmail.com>
2023-04-17 18:46:47 +0900

    enemy: fly

---
+++ enemies.js
@@ -0,0 +1,66 @@
+import { Sitting, Running, Jumping, Falling } from "./playerState.js";
+
+class Enemy {
+  constructor() {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20; // 애니메이션 속도 컨트롤(클수록 빠름)
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+    this.markedForDeletion = false;
+  }
+  update(deltaTime) {
+    // movement
+    this.x -= this.speedX + this.game.speed;
+    this.y += this.speedY;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+
+    // check if off screen
+    if (this.x + this.width < 0) this.markedForDeletion = true;
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      0,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+export class FlyingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+    this.x = this.game.width + Math.random() * this.game.width * 0.5;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.speedX = Math.random() + 1;
+    this.speedY = 0;
+    this.maxFrame = 5;
+    this.image = document.getElementById("enemy_fly");
+  }
+  update(deltaTime) {
+    super.update(deltaTime);
+  }
+  // draw() {
+  //   console.log("fsdkfj");
+  //   // super.draw();
+  // }
+}
+
+export class GroundEnemy extends Enemy {}
+
+export class ClimbingEnemy extends Enemy {}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
+import { FlyingEnemy, GroundEnemy, ClimbingEnemy } from "./enemies.js";
 
 window.addEventListener("load", () => {
   // const loading = document.getElementById("loading");
@@ -20,14 +21,36 @@ window.addEventListener("load", () => {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      // handle Enemies
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+        if (enemy.markedForDeletion)
+          this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      });
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+    }
+    addEnemy() {
+      console.log(this.enemies);
+      this.enemies.push(new FlyingEnemy(this));
     }
   }
 
