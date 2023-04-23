1a265ec513ff16f92b91531209621d07696f10b0
김보미 <kk3p1q1@gmail.com>
2023-04-23 17:35:48 +0900

    add particles(WIP)

--- main.js
+++ main.js
@@ -24,11 +24,14 @@ window.addEventListener("load", () => {
       this.input = new InputHandler(this);
       this.UI = new UI(this);
       this.enemies = [];
+      this.particles = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.debug = true;
       this.score = 0;
       this.fontColor = "black";
+      this.player.currentState = this.player.states[0];
+      this.player.currentState.enter();
     }
     update(deltaTime) {
       this.background.update();
@@ -45,12 +48,19 @@ window.addEventListener("load", () => {
         if (enemy.markedForDeletion)
           this.enemies.splice(this.enemies.indexOf(enemy), 1);
       });
+      this.particles.forEach((particle, index) => {
+        particle.update();
+        if (particle.markedForDeletion) this.particles.splice(index, 1);
+      });
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
+      });
+      this.particles.forEach((particle) => {
+        particle.draw(context);
       });
       this.UI.draw(context);
     }
@@ -61,7 +71,6 @@ window.addEventListener("load", () => {
         this.enemies.push(new ClimbingEnemy(this));
       }
       this.enemies.push(new FlyingEnemy(this));
-      // console.log(this.enemies);
     }
   }
 
@@ -77,8 +86,6 @@ window.addEventListener("load", () => {
     game.draw(ctx, 0);
     game.update(deltaTime);
     requestAnimationFrame(animate);
-
-    // drawStatusText(ctx, input, player);
   };
   animate(0);
 });
